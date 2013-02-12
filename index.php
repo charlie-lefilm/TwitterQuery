da9ffd6226c3c1211293cb1f94ac85e09f95a681
Gerard TOKO <gerardtoko@gmail.com>
2013-02-12 19:52:41 +0100

    test twitter

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ class Twitter {
 
 	protected  $results = array();
 
-	public function searchResults( $search = null, $rpp = 50 ) {
+	public function searchResults( $search = null, $rpp = 100 ) {
 
 		require_once('twitteroauth/twitteroauth/twitteroauth.php');
 
@@ -15,37 +15,35 @@ class Twitter {
 		$oauth_token_secret = '7PKqQ5qdRe9ZfeV9EyS7VdiDQF0k9aHu1DUwmWxgK4'; //Provide your oAuth Token Secret
 
 		$connection = new TwitterOAuth($consumer_key, $consumer_secret, $oauth_token, $oauth_token_secret);
-
 		$query = "https://api.twitter.com/1.1/search/tweets.json?q=" . urlencode( $search ) . "&rpp=$rpp&include_entities=true";
 		$content = $connection->get($query);
 
-		print_r($content);
-		exit;
+		$memcache_obj = new Memcache;
+		$memcache_obj->connect('memcache_host', 11211);
 
-		if(!empty($content->statuses)){
+		#$memcache_obj->delete('tweet_charlie');
 
-			foreach ($content->statuses as $key_tweet => $value_tweet) {
-				if(!empty($value_tweet->entities->media)){
-					foreach ($value_tweet->entities->media as $key_media => $value_media) {
-						if(!empty($value_media->media_url)){
-								$this->results[] = $value_media->media_url;
+		if($data = $memcache_obj->get('tweet_charlie')){
+			$this->results = unserialize($memcache_obj->get('tweet_charlie'));
+		}else{
+			if(!empty($content->statuses)){
+				foreach ($content->statuses as $key_tweet => $value_tweet) {
+					if(!empty($value_tweet->entities->media)){
+						foreach ($value_tweet->entities->media as $key_media => $value_media) {
+							if(!empty($value_media->media_url)){
+									$this->results[] = $value_media->media_url;
+							}
 						}
 					}
 				}
+				$memcache_obj->add('tweet_charlie', serialize($this->results), false, 2592000);
 			}
-			exit;
-			$this->searchResults("charlielefilm", $rpp + 50);
-		}else{
-			return $this->results;
 		}
+
+		return $this->results;
 	}
 
 }
 
 $twitter = new Twitter();
-if(!empty($_GET["debug"])){
-	echo "<pre>";
-	echo $twitter->searchResults("charlielefilm");
-}else{
-	echo json_encode($twitter->searchResults("charlielefilm"));
-}
+echo json_encode($twitter->searchResults("charlielefilm"));
