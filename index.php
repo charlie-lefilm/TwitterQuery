b5e939bf64c6dc00cf42106ea9a6f5a91c6e1777
Gerard TOKO <gerardtoko@gmail.com>
2013-02-12 20:44:25 +0100

    test twitter

--- index.php
+++ index.php
@@ -9,19 +9,17 @@ class Twitter {
 
 		require_once('twitteroauth/twitteroauth/twitteroauth.php');
 
-		$consumer_key='rD0JPpDsScVVjv30SCtllg'; //Provide your application consumer key
-		$consumer_secret='6HcwZYe3fD0VLyCL8UcOEQg4rdIUjGltGWKhsc'; //Provide your application consumer secret
-		$oauth_token = '1170521828-ve33ya1u1TwmfZecl5LuvST8KLXC6lcgDAfUkwy'; //Provide your oAuth Token
-		$oauth_token_secret = '7PKqQ5qdRe9ZfeV9EyS7VdiDQF0k9aHu1DUwmWxgK4'; //Provide your oAuth Token Secret
+		$consumer_key='rD0JPpDsScVVjv30SCtllg';
+		$consumer_secret='6HcwZYe3fD0VLyCL8UcOEQg4rdIUjGltGWKhsc';
+		$oauth_token = '1170521828-ve33ya1u1TwmfZecl5LuvST8KLXC6lcgDAfUkwy';
+		$oauth_token_secret = '7PKqQ5qdRe9ZfeV9EyS7VdiDQF0k9aHu1DUwmWxgK4';
 
 		$connection = new TwitterOAuth($consumer_key, $consumer_secret, $oauth_token, $oauth_token_secret);
 		$query = "https://api.twitter.com/1.1/search/tweets.json?q=" . urlencode( $search ) . "&rpp=$rpp&include_entities=true";
 		$content = $connection->get($query);
 
-		$memcache_obj = new Memcache;
+		$memcache_obj = new Memcache();
 		$memcache_obj->connect('localhost', 11211);
-
-		#$memcache_obj->delete('tweet_charlie');
 
 		if($data = $memcache_obj->get('tweet_charlie')){
 			$this->results = unserialize($memcache_obj->get('tweet_charlie'));
