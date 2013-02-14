89fd1da041b0a8d7a01952de41a5056051a40d76
Gerard TOKO <gerardtoko@gmail.com>
2013-02-14 10:13:44 +0100

    test twitter

--- index.php
+++ index.php
@@ -22,8 +22,6 @@ class Twitter {
 		$memcache_obj = new Memcache();
 		$memcache_obj->connect('localhost', 11211);
 
-		$memcache_obj->delete('tweet_charlie');
-		
 		if($data = $memcache_obj->get('tweet_charlie')){
 			$this->results = unserialize($memcache_obj->get('tweet_charlie'));
 		}else{
